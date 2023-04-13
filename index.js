a9c74c8f7f13638d25c70b34b8b517b0ca5bd43c
kwamebrowne <theeecryptoking@gmail.com>
2023-04-12 21:24:45 -0500

    Finished visual Flair

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+//1. 
+function saturdayFun(arg1 = 'roller-skate') {
+    return `This Saturday, I want to ${arg1}!`
+}
+
+
+
+//2.Implement a function expression called mondayWork. The function should return a String like "This Monday, I will ... ." 
+//Fill in the ... with the activity that's passed in as the first parameter. If nothing is passed in, default to "go to the office". Run npm test to verify you've gotten this set of tests passing before continuing with the lesson.
+function mondayWork(toDo = 'go to the office'){
+    return `This Monday, I will ${toDo}.`//Backtick used instead of quotation 
+}
+
+
+
+//3. 
+function wrapAdjective(arg1 = '*') {
+    return function(param1 = 'special') {
+        return `You are ${arg1}${param1}${arg1}!`
+    } 
+
+}
